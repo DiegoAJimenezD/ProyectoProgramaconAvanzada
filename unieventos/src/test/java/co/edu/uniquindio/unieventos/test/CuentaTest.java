7a3914b25e772afd49c2648151e553226f1a267b
andyfonseca2003 <anderson.fonsecal@uqvirtual.edu.co>
2024-09-11 19:15:02 -0500

    Crear CuentaTest
    Se crea la clase cuenta Test

---
+++ unieventos/src/test/java/co/edu/uniquindio/unieventos/test/CuentaTest.java
@@ -0,0 +1,16 @@
+package co.edu.uniquindio.unieventos.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@SpringBootTest
+public class CuentaTest {
+
+    @Autowired
+    private CuentaRepo cuentaRepo;
+
+}
