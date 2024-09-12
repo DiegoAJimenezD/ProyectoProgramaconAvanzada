ab6cbf8daa1726004ade3735e6351b2932a473b0
andyfonseca2003 <anderson.fonsecal@uqvirtual.edu.co>
2024-09-11 19:31:56 -0500

    Actualizacion CuentaTest
    Se crea el primer metodo

--- unieventos/src/test/java/co/edu/uniquindio/unieventos/test/CuentaTest.java
+++ unieventos/src/test/java/co/edu/uniquindio/unieventos/test/CuentaTest.java
@@ -1,8 +1,15 @@
 package co.edu.uniquindio.unieventos.test;
 
+import co.edu.uniquindio.unieventos.modelo.Cuenta;
+import co.edu.uniquindio.unieventos.modelo.EstadoCuenta;
+import co.edu.uniquindio.unieventos.modelo.Rol;
+import co.edu.uniquindio.unieventos.modelo.Usuario;
+import co.edu.uniquindio.unieventos.repositorios.CuentaRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -12,5 +19,32 @@ public class CuentaTest {
 
     @Autowired
     private CuentaRepo cuentaRepo;
+
+    @Test
+    public void registrarTest(){
+        //Creamos la cuenta con sus propiedades
+        Cuenta cuenta = Cuenta.builder()
+                .email("juanito@email.com")
+                .password("123456")
+                .fechaRegistro(LocalDateTime.now())
+                .estado(EstadoCuenta.ACTIVO)
+                .usuario(
+                        Usuario.builder()
+                                .cedula("123")
+                                .nombre("Juanito")
+                                .direccion("Calle 123")
+                                .telefono("121212").build()
+                )
+                .rol(Rol.CLIENTE).build();
+
+
+        //Guardamos la cuenta del usuario en la base de datos
+        Cuenta cuentaCreada = cuentaRepo.save(cuenta);
+
+
+        //Verificamos que se haya guardado validando que no sea null
+        assertNotNull(cuentaCreada);
+    }
+
 
 }
