95bbe969bbdad4e8b12c9250ac5ffda47e705a72
DiEGO <111940173+DiegoAJimenezD@users.noreply.github.com>
2024-09-11 19:16:06 -0500

    Actualización de los repositorios
    Se le agregó una línea a los repositorios que nos hacia falta

--- unieventos/src/main/java/co/edu/uniquindio/unieventos/repositorios/CuentaRepo.java
+++ unieventos/src/main/java/co/edu/uniquindio/unieventos/repositorios/CuentaRepo.java
@@ -2,6 +2,8 @@ package co.edu.uniquindio.unieventos.repositorios;
 
 import co.edu.uniquindio.unieventos.modelo.Cuenta;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CuentaRepo extends MongoRepository<Cuenta, String> {
 }
--- unieventos/src/main/java/co/edu/uniquindio/unieventos/repositorios/UsuarioRepo.java
+++ unieventos/src/main/java/co/edu/uniquindio/unieventos/repositorios/UsuarioRepo.java
@@ -2,6 +2,8 @@ package co.edu.uniquindio.unieventos.repositorios;
 
 import co.edu.uniquindio.unieventos.modelo.Usuario;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UsuarioRepo  extends MongoRepository<Usuario, String> {
 }
