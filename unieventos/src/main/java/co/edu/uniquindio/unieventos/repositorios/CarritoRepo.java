d8c74de40fc630d0971cf927aae1e02f7e7a6111
DiEGO <111940173+DiegoAJimenezD@users.noreply.github.com>
2024-09-11 19:16:06 -0500

    Actualización de los repositorios
    Se le agregó una línea a los repositorios que nos hacia falta

--- unieventos/src/main/java/co/edu/uniquindio/unieventos/repositorios/CarritoRepo.java
+++ unieventos/src/main/java/co/edu/uniquindio/unieventos/repositorios/CarritoRepo.java
@@ -2,6 +2,8 @@ package co.edu.uniquindio.unieventos.repositorios;
 
 import co.edu.uniquindio.unieventos.modelo.Carrito;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CarritoRepo  extends MongoRepository<Carrito, String> {
 }
