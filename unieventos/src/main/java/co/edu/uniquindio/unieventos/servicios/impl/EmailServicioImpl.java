ce7370b50c33b3b92d814348697f58878ec5c885
DiEGO <111940173+DiegoAJimenezD@users.noreply.github.com>
2024-09-25 00:01:07 -0500

    Creación de el envío de correos electronicos
    Se crearon las clases para el envío de correos electrónicos

---
+++ unieventos/src/main/java/co/edu/uniquindio/unieventos/servicios/impl/EmailServicioImpl.java
@@ -0,0 +1,49 @@
+package co.edu.uniquindio.unieventos.servicios.impl;
+
+import co.edu.uniquindio.unieventos.dto.EmailDTO;
+import co.edu.uniquindio.unieventos.servicios.interfaces.EmailServicio;
+import org.simplejavamail.api.email.Email;
+import org.simplejavamail.api.mailer.Mailer;
+import org.simplejavamail.api.mailer.config.TransportStrategy;
+import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.mailer.MailerBuilder;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class EmailServicioImpl implements EmailServicio {
+
+
+    @Override
+    @Async
+    public void enviarCorreo(EmailDTO emailDTO) throws Exception {
+
+
+        Email email = EmailBuilder.startingBlank()
+                .from("SMTP_USERNAME")
+                .to(emailDTO.destinatario())
+                .withSubject(emailDTO.asunto())
+                .withPlainText(emailDTO.cuerpo())
+                .buildEmail();
+
+
+        try (Mailer mailer = MailerBuilder
+                .withSMTPServer("smtp.gmail.com", 587, "ragnar07kxl@gmail.com", "akyg ztra futh pqlq")
+                .withTransportStrategy(TransportStrategy.SMTP_TLS)
+                .withDebugLogging(true)
+                .buildMailer()) {
+
+//Para enviar correos, poner este metodo
+            //emailServicio.enviarCorreo( new EmailDTO("Asunto", "Cuerpo mensaje", "Correo destino") );
+
+            
+            mailer.sendMail(email);
+        }
+
+
+    }
+
+
+}
+
---
+++ unieventos/src/main/java/co/edu/uniquindio/unieventos/servicios/interfaces/EmailServicio.java
@@ -0,0 +1,7 @@
+package co.edu.uniquindio.unieventos.servicios.interfaces;
+
+import co.edu.uniquindio.unieventos.dto.EmailDTO;
+
+public interface EmailServicio {
+    void enviarCorreo(EmailDTO emailDTO) throws Exception;
+}
