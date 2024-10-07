05bfe44099fb4a6831a96f6fc93f7a826a8ccd97
andyfonseca2003 <anderson.fonsecal@uqvirtual.edu.co>
2024-10-07 16:19:22 -0500

    Correción DTO
    Se revisa y se corrigen los DTO

--- unieventos/src/main/java/co/edu/uniquindio/unieventos/dto/Evento/EditarEventoDTO.java
+++ unieventos/src/main/java/co/edu/uniquindio/unieventos/dto/Evento/EditarEventoDTO.java
@@ -3,6 +3,7 @@ package co.edu.uniquindio.unieventos.dto.Evento;
 import java.time.LocalDateTime;
 
 public record EditarEventoDTO(
+        String id,
         String nombre,
         LocalDateTime fecha,
         String categoria,
